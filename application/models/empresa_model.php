0e12f8633694c7bf9340cce5b666d000264a4301
kelverty <kelverty.ktec@gmail.com>
2014-06-26 11:12:40 -0300

    funções no cadastro de atividade nas empresa

--- application/models/empresa_model.php
+++ application/models/empresa_model.php
@@ -2,18 +2,31 @@
 class Empresa_model extends CI_Model {
 
 
-public function listar($where=array()){
-	return $this->db->get_where("Empresa", $where)->result_array();
+	public function listar($where=array()){
+		return $this->db->get_where("Empresa", $where)->result_array();
 
 
-} 
-public function listarAtividade($where=array()){
-	return $this->db->get_where("AtividadeEmpresa", $where)->result_array();
+	} 
+	public function listarAtividade($where=array()){
+		return $this->db->get_where("AtividadeEmpresa", $where)->result_array();
 
 
-} 
+	} 
 
-public function salvar ($empresa){
+	public function listarAtividadeJoin($where=array()){
+
+		$this->db->select("AtividadeEmpresa.*, Empresa.razaoSocial,Setor.descricao as setorDescricao , Atividade.descricao as atividadeDescricao");
+		$this->db->from("AtividadeEmpresa");
+		$this->db->join("Empresa", "Empresa.idEmpresa = AtividadeEmpresa.Empresa_idEmpresa");
+		$this->db->join("Setor", "Setor.idSetor = AtividadeEmpresa.Setor_idSetor");
+		$this->db->join("Atividade", "Atividade.idAtividade = AtividadeEmpresa.Atividade_idAtividade");
+		$this->db->where($where);
+
+		return $this->db->get()->result_array();
+	}
+
+
+	public function salvar ($empresa){
 		if ($empresa['idEmpresa']>0){
 			$this->db->where("idEmpresa",$empresa['idEmpresa']);
 			$this->db->update("Empresa",$empresa);
@@ -24,9 +37,22 @@ public function salvar ($empresa){
 
 	}
 	public function excluir($id){
-			$this->db->where("idEmpresa",$id);
+		$this->db->where("idEmpresa",$id);
 		$this->db->delete('Empresa'); 
 
 	}
+
+	public function cadAtividade($atividade){
+
+			if ($atividade['idAtividadeEmpresa']>0){
+			$this->db->where("idAtividadeEmpresa",$atividade['idAtividadeEmpresa']);
+			$this->db->update("AtividadeEmpresa",$atividade);
+
+		}else{
+		$this->db->insert("AtividadeEmpresa",$atividade);
+
+}
+	}
+
 
 }
