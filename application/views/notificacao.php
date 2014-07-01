b132931262dbf252f878c9d1bff9a61afbeff3be
jrluiscarvalho <jrluiscarvalho@mgail.com>
2014-07-01 10:41:54 -0300

    acabamento layout

--- application/views/notificacao.php
+++ application/views/notificacao.php
@@ -1,3 +1,3 @@
-<div class="col-md-9 col-md-offset-1">
+<div class="col-md-10">
 	<p class="alert-success">  <?php echo $this->session->flashdata('success'); ?></p>
 		<p class="alert-danger">  <?php echo $this->session->flashdata('danger'); ?></p>
