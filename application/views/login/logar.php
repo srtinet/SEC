712aa4fc8d4347ff4db207d0afe5fb22bbd52778
jrluiscarvalho <jrluiscarvalho@mgail.com>
2014-07-01 13:47:15 -0300

    acabamento tela login

--- application/views/login/logar.php
+++ application/views/login/logar.php
@@ -1,14 +1,14 @@
-	
 <p class="alert-success">  <?php echo $this->session->flashdata('success'); ?></p>
 <p class="alert-danger">  <?php echo $this->session->flashdata('danger'); ?></p>
-
 <h1>Login</h1>
-<?php 
-echo form_open("login/autenticar");
-echo inputText("usuario", "Email");
-echo inputPass("senha", "Senha");
-echo form_button(array("class"=>"btn btn-primary","content"=>"Login","type"=>"submit"));
-echo form_close();
-
-
-?>
+<div class="row telaLogin">
+	<div class="col-lg-5 col-lg-offset-4">
+		<?php
+		echo form_open("login/autenticar");
+		echo inputText("usuario", "Email");
+		echo inputPass("senha", "Senha");
+		echo form_button(array("class"=>"btn btn-primary","content"=>"Login","type"=>"submit"));
+		echo form_close();
+		?>
+	</div>
+</div>
